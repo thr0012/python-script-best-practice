cadfb3639cf813ea54361deb7dfa85af1725f30d
Thais <thr0012@auburn.edu>
2026-03-02 15:31:01 -0600

    Write modules and not script

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,19 +4,23 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
+def get_smallest_prime_factor(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return i
+    return None
 
-x = None
-for i in range(2, n):
-    if (n % i) == 0:
-        x = i
-        break
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
+    n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + ": Expecting a positive integer")
+    
+    smallest_prime_factor = get_smallest_prime_factor(n)
+smallest_prime_factor = get_smallest_prime_factor(n)
 
-if x is None:
+if smallest_prime_factor is None:
     print(n)
 else:
-    print(x)
+    print(smallest_prime_factor)
